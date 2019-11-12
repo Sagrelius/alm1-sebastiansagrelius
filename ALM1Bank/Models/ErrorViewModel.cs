5dc14dcb47b541f2596b6454270d6a45b4718b23
Sebastian Sagrelius <sagrelius@live.se>
2019-11-12 13:46:28 +0100

    Add project files.

---
+++ ALM1Bank/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ALM1Bank.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
