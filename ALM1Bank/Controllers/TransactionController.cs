e1e9269baf8ce7f6a7a8bc281a83ea4f4faee45e
Sebastian Sagrelius <sagrelius@live.se>
2019-11-14 12:46:15 +0100

    Add deposit and withdraw methods to bankrepository. Add UnitTests to bankrepository methods

---
+++ ALM1Bank/Controllers/TransactionController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ALM1Bank.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ALM1Bank.Controllers
+{
+    public class TransactionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Withdraw(decimal amount, int accountID)
+        {
+            var repo = new BankRepository();
+            var account = repo.Withdraw(amount, accountID);
+
+            if (account)
+            {
+                return View("~/Views/Home/Index.cshtml", repo);
+            }
+            ViewBag.Message = "Något blev fel. Vänligen försök igen.";
+            return View("Index");
+        }
+
+        public IActionResult Deposit(decimal amount, int accountID)
+        {
+            var repo = new BankRepository();
+            var account = repo.Deposit(amount, accountID);
+
+            if (account)
+            {
+                return View("~/Views/Home/Index.cshtml", repo);
+            }
+
+            ViewBag.Message = "Något blev fel. Vänligen försök igen.";
+            return View("Index");
+        }
+    }
+}
